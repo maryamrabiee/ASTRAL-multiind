a790d8687792d03a44a27f3ebb18505017b949c9
esayyari <esayyari@eng.ucsd.edu>
2017-01-24 11:00:14 -0800

    adding new figures and data

--- R/dataInfo.R
+++ R/dataInfo.R
@@ -15,5 +15,6 @@ h$V7<-as.numeric(as.character(factor(h$V7)))
 h$rf<-(h$V3+h$V6)/(h$V2+h$V5)
 ggplot(data=h,aes(x=rf))+geom_density(fill='red',alpha=0.5,adjust=1.5)+theme_bw()+xlab('RF Distance (True vs Estimated)')+ylab('Density')
 
-d<-read.csv('result2.csv',sep=" ",header=F)
+d<-read.csv('../data/result2.csv',sep=" ",header=F)
 qplot(data=d,x=reorder(as.factor(V1),V5),y=V5,color=as.factor(V2),geom='jitter')+theme_bw()+xlab('replicate')+ylab('FN error')+theme(axis.ticks = element_blank(), axis.text.x = element_text(angle = 90, hjust = 0, colour = "grey50"))+scale_color_brewer(name="Number of rounds",palette="Dark2")
+ggsave('EstimatedGeneTrees.pdf')
