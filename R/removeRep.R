9f113552df67e4cd2e3ebeaa107f4196e34c0870
esayyari <esayyari@eng.ucsd.edu>
2017-01-28 11:53:14 -0800

    adding new figures and data

--- R/removeRep.R
+++ R/removeRep.R
@@ -15,7 +15,7 @@ print(h[h$medianSeqLen>2000,1])
 stat<-merge(x=h,y=q,by.x="V1",by.y="Replicate")
 
 
-f<-read.csv('../data/estimatedBrLengths.csv',sep=" ",header=F)
+f<-read.csv('../data/estimatedBrLengths.csv.5.0.3',sep=" ",header=F)
 br<-merge(f,q,by.x="V1",by.y="Replicate",all=TRUE)
 br$true<-br$V2/(br$Haploid_efective_population_size)
 br$logl <- abs(log10(br[,3])-log10(br[,14]))
@@ -30,10 +30,10 @@ ggplot(data=br[br$V3>0,],aes(x=log10(true),y=log10(V3),color=Number_of_Genes))+
 ggsave('../figures/BL-alpha0.5.pdf',width=8,height=6.2)
 
 br$labelG <- "H"
-br[br$Number_of_Genes>770,]$labelG <- "High NumGenes"
+br[br$Number_of_Genes>770,]$labelG <- "#Genes >770 (3rd quantile)"
 #br[br$Number_of_Genes<=750 & br$Number_of_Genes>500,]$labelG <-"Med NumGenes"
-br[br$Number_of_Genes<=770,]$labelG <- "Low NumGenes"
-br$labelG <- factor(br$labelG, levels = c("Low NumGenes", "Med NumGenes","High NumGenes" ))
+br[br$Number_of_Genes<=770,]$labelG <- "#Genes <=770 (3rd quantile)"
+br$labelG <- factor(br$labelG, levels = c("#Genes <=770 (3rd quantile)", "#Genes >770 (3rd quantile)" ))
 
 ggplot(data=br[br$V3>0,],aes(x=log10(true),y=log10(V3)))+facet_wrap(~labelG)+
   geom_point(alpha=0.1,size=1.5)+theme_bw()+
@@ -41,7 +41,8 @@ ggplot(data=br[br$V3>0,],aes(x=log10(true),y=log10(V3)))+facet_wrap(~labelG)+
   xlab("true branch length (log scale)")+
   geom_abline(linetype=2)+ ylab("Estimated branch length (log scale)")+
   coord_cartesian(ylim=c(-5.3,2.3),xlim=c(-5.3,2.3))+geom_smooth()
-ggsave('../figures/BL-diffLevelsNumGenes.pdf',height=9.5, width=5)
+ggsave('./BL-diffLevelsNumGenes.png',width=9.5, height =5)
+ggsave('./BL-diffLevelsNumGenes.pdf',width=9.5, height =5)
 
 
 bla=recast(melt(data=br[br$V3!=0,c(1,15,16,17)],
