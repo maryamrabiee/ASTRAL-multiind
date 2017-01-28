810769a78ab6b0373de3bfaba9688548247d45c6
esayyari <esayyari@eng.ucsd.edu>
2017-01-28 13:12:48 -0800

    adding new figures and data

--- R/removeRep.R
+++ R/removeRep.R
@@ -2,20 +2,20 @@ require(reshape2)
 require(ggplot2)
 require(Hmisc)
 
-d <- read.csv('../data/seqLength.csv',header=F,sep=" ")
+d <- read.csv('../data/seqLength.csv.final',header=F,sep=" ")
 
 d$V2 <- as.numeric(as.character(d$V2))
 
 h <- dcast(d, V1~.,value.var="V2",fun.aggregate=median)
 
 names(h) <- c("V1","medianSeqLen")
-q<-read.csv('../data/parameter.log.info1',header=T,sep=" ")
+q<-read.csv('../data/parameter.log.info1.final',header=T,sep=" ")
 print(h[h$medianSeqLen<300,1])
 print(h[h$medianSeqLen>2000,1])
 stat<-merge(x=h,y=q,by.x="V1",by.y="Replicate")
 
 
-f<-read.csv('../data/estimatedBrLengths.csv.5.0.3',sep=" ",header=F)
+f<-read.csv('../data/estimatedBrLengths.csv.5.0.3.final',sep=" ",header=F)
 br<-merge(f,q,by.x="V1",by.y="Replicate",all=TRUE)
 br$true<-br$V2/(br$Haploid_efective_population_size)
 br$logl <- abs(log10(br[,3])-log10(br[,14]))
@@ -45,10 +45,10 @@ ggsave('./BL-diffLevelsNumGenes.png',width=9.5, height =5)
 ggsave('./BL-diffLevelsNumGenes.pdf',width=9.5, height =5)
 
 
-bla=recast(melt(data=br[br$V3!=0,c(1,15,16,17)],
-                id.vars=c("V1","labelG")),.~variable,
+bla=recast(melt(data=br[br$V3!=0,c(1,17,18,19)],
+                id.vars=c("V1","labelG")),.~variable+labelG,
            measure.var = "value",
            fun.aggregate=function(x)mean(x[!is.infinite(x)]))
-bla[,2:3] = sqrt(bla[,2:3])
+bla[,2:5] = sqrt(bla[,2:5])
 l=latex(format(bla,digits=3),file="../figures/bl.tex",rowname = NULL)
-d<-read.csv('seqLength.csv',sep=" ",header=F)
+d<-read.csv('../data/seqLength.csv.final',sep=" ",header=F)
