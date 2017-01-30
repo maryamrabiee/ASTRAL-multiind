8ccd28341f83a394787c4d9dd0f39da6409d55ab
Siavash Mirarab <smirarab@gmail.com>
2017-01-29 18:48:13 -0800

    update script

--- R/st.R
+++ R/st.R
@@ -1,7 +1,7 @@
 require(ggplot2)
 require(reshape2)
 
-d=read.csv("allresults.stat",sep=" ",head=F)
+d=read.csv("accuracy.stat.truegenes",sep=" ",head=F)
 
 h=read.csv('gtError-simphy-multiind.csv',sep=" ",header=F)
 h$V2<-as.numeric(as.character(factor(h$V2)))
@@ -15,12 +15,11 @@ h$rf<-(h$V3+h$V6)/(h$V2+h$V5)
 agt=dcast(V1~., data=h[,c(1,9)],fun.aggregate = function(x) mean (x,na.rm = T))
 names(agt)[2] = "RF"
 
-a = merge(merge(d,agt,by.x="V2",by.y="V1"),read.csv('parameter.log.info1',header = T,sep= ' '),by.x="V2", by.y="Replicate")
-names(a)[6] = "stError"
+a = merge(merge(d,agt,by.x="V1",by.y="V1"),read.csv('parameter.log.info1.final',header = T,sep= ' '),by.x="V1", by.y="Replicate")
+names(a)[5] = "stError"
 names(a)[1] = "Replicate"
-names(a)[2] = "Method"
 
-ggplot(aes(x=Number_of_Genes,y=stError, color=log(3/2*(TrueQscore-1/3))), data=a[a$Method == "true",])+
+ggplot(aes(x=Number_of_Genes,y=stError, color=log(3/2*(TrueQscore-1/3))), data=a)+
   geom_point()+theme_bw()+geom_smooth()+theme(legend.position=c(.6,.86),legend.direction = 1)+
   scale_color_continuous(name="Quartet score",breaks=c(-1,-2,-3),labels=format(2/3*exp(-1:-3)+1/3,digits = 2))+
   xlab("# of genes")+ylab("Species tree error (RF distance)")
